4e2fd229929551e80f4f5ff4652d21df37f05860
anonymousvegan <anonymousvegan>
2022-03-12 14:48:58 +0100

    it will work, i promisse

--- project3/app.js
+++ project3/app.js
@@ -1,7 +1,8 @@
 let poljaElem = document.querySelectorAll("#cont > div");
 let polja = [];
-
-for(let i=0; i<4; i++){
+let size = 4;
+let temp;
+for(let i=0; i<size; i++){
     polja[i] = [];
 }
 
@@ -10,14 +11,15 @@ let brojZauzetih = 0;
 function random(){
     if(brojZauzetih < 16){
         let broj =  (Math.floor(Math.random()*2)+1)*2;
-        let x =  (Math.floor(Math.random()*4));
-        let y =  (Math.floor(Math.random()*4));
+        let x =  (Math.floor(Math.random()*size));
+        let y =  (Math.floor(Math.random()*size));
         if(polja[x][y].zauzeto){        
             random();
         }
         else{
             polja[x][y].zauzeto = true;
             polja[x][y].broj = broj;
+            polja[x][y].elem.textContent = broj;
             brojZauzetih++;
         }
     }
@@ -32,21 +34,55 @@ function setup() {
     poljaElem.forEach(polje => {
         polja[i].push({elem: polje, broj: 0,  zauzeto: false})
         j++;
-        if(j==4){
+        if(j==size){
             j=0; 
             i++;
         }
     });
     random()
 }
+let z = 3;
+function canGoUp(i, j){
+    z--;
+    while(z>0)
+        return 1;
+}
 
 document.addEventListener("keyup", (event) => {
     switch(event.key){
         case "ArrowUp":
-            for(let i = 0; i<4; i++){
-                for(let j = 0; j<4; j++){
-                    console.log("here :", polja[i][j].zauzeto);
-                }
+            console.log("gore");
+            //##############
+                for (i=0; i<size; i++){
+                    for(j=0; j<size; j++){
+                        if(polja[i][j].zauzeto){
+                            if(i > 0 ){
+                                if(polja[i-1][j].zauzeto != true){
+                                    while(canGoUp(i, j)){
+                                        temp =  polja[i][j].broj;
+                                        console.log(temp);
+                                        polja[i-1][j].elem.textContent = temp;
+                                        polja[i-1][j].broj = temp;
+                                        polja[i-1][j].zauzeto = true;
+                                        polja[i][j].elem.textContent = "";
+                                        polja[i][j].broj = 0;
+                                        polja[i][j].zauzeto = false;
+                                    }
+                                }
+                                else if(polja[i-1][j].broj == polja[i][j].broj){
+                                    temp =  polja[i][j].broj;
+                                    console.log("drugi slučaj:",temp);
+                                    polja[i-1][j].elem.textContent = temp*2;
+                                    polja[i-1][j].broj = temp*2;
+                                    polja[i-1][j].zauzeto = true;
+                                    polja[i][j].elem.textContent = "";
+                                    polja[i][j].broj = 0;
+                                    polja[i][j].zauzeto = false;
+                                    brojZauzetih--;
+                                }
+                            }
+                        }
+                    }
             }
             random();
             break;
