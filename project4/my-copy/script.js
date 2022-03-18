7224b1610bf1bede5b13bdde265a2a3fc830e62d
anonymousvegan <majjunjulavgust@gmail.com>
2022-03-18 21:28:14 +0100

    optimisations

--- project4/my-copy/script.js
+++ project4/my-copy/script.js
@@ -1,6 +1,6 @@
 
 const elem = document.getElementById("top-arrow")
-window.addEventListener("scroll", (event) => {
+window.addEventListener("scroll", () => {
     let top  = window.pageYOffset || document.documentElement.scrollTop
     if(top > 180){
         elem.classList.add("show");
