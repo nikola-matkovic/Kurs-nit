9257d5b06654b18afd484519d2f2fd530a80fc85
anonymousvegan <majjunjulavgust@gmail.com>
2022-03-20 17:51:48 +0100

    price cards are finished, bug fixes needed

--- project4/my-copy/pricing.php
+++ project4/my-copy/pricing.php
@@ -4,20 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Trainers - Mentor Bootstrap Template - Index</title>
+    <title>Pricing - Mentor Bootstrap Template - Index</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="responsive.css">
-    <link rel="stylesheet" href="mentors.css">
+    <link rel="stylesheet" href="pricing.css">
     <link rel="icon" href="favicon.png">
     <script src="script.js" defer></script>
 </head>
 <body>
     <?php
-        $header = "Trainers";
+        $header = "Pricing";
         include "nav.html";
         include "description.php";
-        include "mentors.html";
+        include "price.html";
         include "footer.html";
         include "arrow.html";
     ?>
