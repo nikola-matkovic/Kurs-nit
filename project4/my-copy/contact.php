d4f0ab69a8c0b5b5f9a2827e1ecce1c8f64d16d6
anonymousvegan <majjunjulavgust@gmail.com>
2022-03-21 22:33:13 +0100

    added contact page

---
+++ project4/my-copy/contact.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact - Mentor Bootstrap Template - Index</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="contact.css">
+    <link rel="icon" href="favicon.png">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <?php
+        $header = "Contact Us";
+        include "nav.html";
+        include "description.php";
+        include "contact.html";
+        include "footer.html";
+        include "arrow.html";
+    ?>
+</body>
+</html>
