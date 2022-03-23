b3ed76d831af92aadd0a5797aaa37bad0af59db0
anonymousvegan <majjunjulavgust@gmail.com>
2022-03-23 20:01:44 +0100

    finished secound part of page

---
+++ project4/my-copy/course-details.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course Details - Mentor Bootstrap Template</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
+    <link rel="stylesheet" href="course-details.css">
+    <link rel="icon" href="favicon.png">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <?php
+        $header = "Course Details";
+        include "nav.html";
+        include "description.php";
+        include "course-details.html";
+        include "footer.html";
+        include "arrow.html";
+    ?>
+    <script>
+        window.onload=()=>{
+            active(3);
+        };
+    </script>
+</body>
+</html>
