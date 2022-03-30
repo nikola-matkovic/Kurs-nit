0595b6de6244640013b6abdce36986ac2c6b87e7
anonymousvegan <majjunjulavgust@gmail.com>
2022-03-30 19:36:35 +0200

    I'm traying make animations

--- project4/my-copy/index.php
+++ project4/my-copy/index.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mentor Bootstrap Template - Index</title>
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <script src="script.js"  defer></script>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="responsive.css">
     <link rel="icon" href="favicon.png">
-    <link rel="stylesheet" href="animations.css">
-    <script src="script.js"  defer></script>
 </head>
 <body>
     <?php
@@ -30,6 +30,10 @@
         window.onload=()=>{
             active(1);
         };
+    </script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script>
+        AOS.init();
     </script>
 </body>
 </html>
