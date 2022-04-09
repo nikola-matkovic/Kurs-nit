52a0e395a55d4af6d2e175a1cf57b97640ffa99e
anonymousvegan <majjunjulavgust@gmail.com>
2022-04-09 17:55:56 +0200

    better way

--- class21/zadatak1.js
+++ class21/zadatak1.js
@@ -1,10 +1,12 @@
 var a = ['a','b', 'c', 'd']
 var b = ['a','b', 'f']
+var c = [];
 
 for(let i = 0; i<a.length; i++){
     for(let j = 0; j<b.length; j++){
         if(a[i] == b[j]){
-            console.log(a[i]);
+            c.push(a[i]);
         }
     }
 }
+console.log(c);
