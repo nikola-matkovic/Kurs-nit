c86ccb8961500287dad9a00ee3d22294473fdf36
anonymousvegan <majjunjulavgust@gmail.com>
2022-04-12 17:34:33 +0200

    concet

--- class21/zadatak1.js
+++ class21/zadatak1.js
@@ -2,11 +2,21 @@ var a = ['a','b', 'c', 'd']
 var b = ['a','b', 'f']
 var c = [];
 
+let found = false;
 for(let i = 0; i<a.length; i++){
-    for(let j = 0; j<b.length; j++){
-        if(a[i] == b[j]){
-            c.push(a[i]);
+    c.push(a[i]);
+}
+for(let i = 0; i<b.length; i++){
+    c.push(b[i]);
+}
+for(let i = 0; i<c.length; i++){
+    for(let j = i+1; j<c.length; j++){
+        if(c[i] === c[j]){
+            c.splice(j, 1);
+        }
+        else{
+            
         }
     }
 }
-console.log(c);
+  console.log(c)
---
+++ class22/zadatak1.js
@@ -0,0 +1,14 @@
+function concatarrays(array1, array1){
+    let c = [];
+    for(let i = 0; i<a.length; i++){
+        c.push(a[i]);
+    }
+    for(let i = 0; i<b.length; i++){
+        c.push(b[i]);
+    }
+    return c;
+}
+a = [1,2,3];
+b = [4,5,6];
+let d = concatarrays(a, b);
+console.log(d);
