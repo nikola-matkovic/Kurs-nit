0719fb334ff670d33f63e6ef227fe7dcd9daa4ee
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-14 13:52:24 +0200

    largest string algorithms

---
+++ basic-algorithm-learning/largest-of-subarray.js
@@ -0,0 +1,5 @@
+function largestOfFour(arr) {
+    console.log(Math.max(arr.map((subArr) => Math.max(...subArr)
+  }
+  
+  largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
