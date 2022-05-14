878d8d5372712c8559f61868840d7f888669818e
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-14 23:00:17 +0200

    reši

--- class34/index.js
+++ class34/index.js
@@ -3,7 +3,7 @@ const  fn = async (args) => {
     const  author = args[0]
     const response = await fetch((`https://api.quotable.io/random${author ?`?author=${author}` : ""}`))
     const result = await response.json();
-    console.log(result.content + "; author: " + result.author);
+    console.log(result.content + ", author: " + result.author);
 }
 
-fn(["Nikola Tesla"])
+fn(["Buddha"])
