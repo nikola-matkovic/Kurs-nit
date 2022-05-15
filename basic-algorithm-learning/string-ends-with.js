891d232e40d31c0ebfaf99343252a427180e6590
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-15 17:02:54 +0200

    works!!!

--- basic-algorithm-learning/string-ends-with.js
+++ basic-algorithm-learning/string-ends-with.js
@@ -1,14 +1,4 @@
 function confirmEnding(str, target) {
-    console.log(str.length);
-    console.log(target.length);
-    console.log(str[str.length-target.length])
-    j = 0;
-    for(let i = str.length - target.length; i < str.length; i++ ){
-        if(str[i] != target[j]){
-            return false;
-        }
-        j++;
-    }
-    return true;
+    return target == str.slice(-target.length);
 }
 console.log(confirmEnding("Abstraction", "action"));
