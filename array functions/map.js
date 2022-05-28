73aec7efcc4b531641bdf60081fd1fdd3edb6c3b
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-28 14:07:54 +0200

    map callback

---
+++ array functions/map.js
@@ -0,0 +1,12 @@
+Array.prototype.newMap = function (callback){
+    arr = this
+    newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        element = arr[i];
+        newArr.push(callback(element, i, arr));
+    }
+}
+
+let a = [1,2,3,4,5,6,7,8,9,10,11]
+let b = a.map(value => value*10)
+console.log(b)
