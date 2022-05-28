162604444639b1aeec89bebf460911b6b920889d
anonymousvegan <majjunjulavgust@gmail.com>
2022-05-28 14:10:19 +0200

    map working

--- array functions/map.js
+++ array functions/map.js
@@ -5,8 +5,9 @@ Array.prototype.newMap = function (callback){
         element = arr[i];
         newArr.push(callback(element, i, arr));
     }
+    return newArr;
 }
 
 let a = [1,2,3,4,5,6,7,8,9,10,11]
-let b = a.map(value => value*10)
+let b = a.newMap(value => value*10)
 console.log(b)
