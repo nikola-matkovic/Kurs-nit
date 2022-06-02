a7a486a090f6d21f2af9a9558b6314f1fda77528
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-02 14:52:27 +0200

    finished this api

--- class39/script.js
+++ class39/script.js
@@ -1,84 +1,49 @@
-const getQuotes = async (page = 1) => {
-    try {
-      const response = await fetch(
-        `https://api.quotable.io/quotes?${page ? `page=${page}` : ""}`
-      );
-  
-      const result = await response.json();
-  
-      return result.results;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  let page = 1;
-  const contentDiv = document.getElementById("content");
-  
-  // const createCard = (author, quote) => {
-  //   return `<div
-  //  style="
-  //    width: 400px;
-  //    min-height: 100px;
-  //    border: 1px solid black;
-  //    margin: 10px;
-  //    padding: 20px;
-  //  "
-  // >
-  //  <p>
-  //    ${quote}
-  //  </p>
-  //  <p>By: ${author}</p>
-  // </div>`;
-  // };
-  
-  // getQuotes().then((result) => {
-  //   result.forEach((quote) => {
-  //     console.log(contentDiv.innerHTML);
-  //     contentDiv.innerHTML =
-  //       contentDiv.innerHTML + createCard(quote.author, quote.content);
-  //   });
-  //   //   contentDiv.appendChild();
-  // });
-  
-  const createCardNode = (author, quote) => {
-    const div = document.createElement("div");
-    div.style = ` width: 400px;
-         min-height: 100px;
-         border: 1px solid black;
-         margin: 10px;
-         padding: 20px;
-       `;
-  
-    const p = document.createElement("p");
-    p.textContent = quote;
-  
-    div.appendChild(p);
-  
-    const p2 = document.createElement("p");
-    p2.textContent = `By: ${author}`;
-  
-    div.appendChild(p2);
-  
-    return div;
-  };
-  
-  // getQuotes().then((result) => {
-  //   result.forEach((quote) => {
-  //     const newCard = createCardNode(quote.author, quote.content);
-  //     contentDiv.appendChild(newCard);
-  //   });
-  // });
-  // const button = document.getElementById("button");
-
-  button.addEventListener("click", load)
-  function load(){
-    page  = parseInt(document.getElementById("page-number").value)
-    contentDiv.innerHTML = ""
-    getQuotes(page).then((result) => {
-      result.forEach((quote) => {
-        const newCard = createCardNode(quote.author, quote.content);
-        contentDiv.appendChild(newCard);
-      });
-    });
+const getQuotes = async () => {
+  let address = createAddress();
+  try {
+    const response = await fetch(address);
+    const result = await response.json();
+    return result.results;
+  } 
+  catch (error) {
+    console.log(error);
   }
+};
+
+function createAddress(){
+  let page  = parseInt(document.getElementById("page-number").value);
+  let sortBy = document.getElementById("sort-by").value;
+  let order = document.getElementById("order").value;
+  let address = "https://api.quotable.io/quotes?";
+  address += `${page ? `page=${page}` : "page=1"}`;
+  address += `${sortBy ? `&sortBy=${sortBy}` : "&sortBy=dateAdded"}`;
+  address += `${order ? `&order=${order}` : "&order=desc"}`;
+  console.log(address);
+  return address;
+}
+
+const contentDiv = document.getElementById("content");
+
+const createCardNode = (author, quote) => {
+  return `
+    <div class="card">
+      <div class="card-body">
+        <p class="card-text"> ${quote} </p>
+      </div>
+  <footer class="blockquote-footer">
+    <small class="text-muted"> ${author} </small>
+  </footer>
+</div>`;
+};
+  
+
+button.addEventListener("click", load)
+function load(){
+  contentDiv.innerHTML = ""
+  getQuotes().then((result) => {
+    result.forEach((quote) => {
+      const newCard = createCardNode(quote.author, quote.content);
+      contentDiv.innerHTML += newCard;
+    });
+  });
+}
