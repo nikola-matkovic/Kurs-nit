2f8acff7e3ed067696ed4c450bfdc9228eb75812
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-07 22:13:08 +0200

    one more algorithm learned

---
+++ basic-algorithm-learning/Where do I Belong.js
@@ -0,0 +1,12 @@
+function getIndexToIns(arr, num) {
+    let arr2 = [...arr].sort( (a, b) => a - b);
+    let i = 0;
+    for(i; i<arr2.length; i++){
+      if(num <= arr2[i]){
+        break;
+      }
+    }
+    return i;
+  }
+  
+  console.log(getIndexToIns([5, 3, 20, 3], 50));
