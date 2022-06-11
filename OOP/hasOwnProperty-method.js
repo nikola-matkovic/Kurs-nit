2f5b0d278b8930c70542130aad002b6007cf168b
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-11 12:11:23 +0200

    has own property method

---
+++ OOP/hasOwnProperty-method.js
@@ -0,0 +1,14 @@
+function Bird(name) {
+    this.name = name;
+    this.numLegs = 2;
+  }
+  
+  let canary = new Bird("Tweety");
+  let ownProps = [];
+  for (let property in canary){
+    if(canary.hasOwnProperty(property)){
+      ownProps.push(property)
+    }
+  }
+  
+  // Only change code below this line
