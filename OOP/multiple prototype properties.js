2ef102bc3fc9aeb90116b5fc4b1081fad1a013dd
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-11 16:38:16 +0200

    more then one property in prototype

---
+++ OOP/multiple prototype properties.js
@@ -0,0 +1,10 @@
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype = {
+    // Only change code below this line
+    numLegs : 4,
+    eat : function(){ return "...."},
+    describe: function(){return "...."}
+  };
