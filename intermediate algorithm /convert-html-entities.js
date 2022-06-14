403d0c33328fac0a2447661885ca8510b230f72b
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-14 15:22:19 +0200

    html entities converting

---
+++ intermediate algorithm /convert-html-entities.js
@@ -0,0 +1,10 @@
+function convertHTML(str) {
+    str = str.replace(/&/g, '&amp;');
+    str = str.replace(/</g, '&lt;');
+    str = str.replace(/>/g, '&gt;');
+    str = str.replace(/"/g, '&quot;');
+    str = str.replace(/'/g, '&apos;');
+    return str;
+}
+  
+console.log(convertHTML("Dolce & Gabbana"));
