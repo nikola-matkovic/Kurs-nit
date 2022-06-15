668f22f507a5361c6bde3be639fa4425ccfbdb9e
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-15 13:15:30 +0200

    manual set constructor

---
+++ OOP/manual-set-constructor.js
@@ -0,0 +1,16 @@
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+// Only change code below this line
+
+
+
+let duck = new Bird();
+let beagle = new Dog();
