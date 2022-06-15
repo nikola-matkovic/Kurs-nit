eac76a22dfa2f4039d4450e9ac57c4c1422e0cc0
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-15 13:40:34 +0200

    iife wiht es6 notation

---
+++ OOP/IIFE module 2.js
@@ -0,0 +1,6 @@
+let funModule = ( () => {
+    return {
+        isCuteMixin : (obj) =>  { obj.isCute = () =>  true; },
+        singMixin : (obj) => {  obj.sing = ()  => { console.log("Singing to an awesome tune");}
+    }
+}})()
