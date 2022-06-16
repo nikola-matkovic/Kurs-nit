c3abfc90a69323a2f0bd48b4d4ffd8d856e9a7fb
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-16 11:52:18 +0200

    split with regex

---
+++ functional-programming/split.js
@@ -0,0 +1,7 @@
+function splitify(str) {
+    // Only change code below this line
+    return str.split(/-|\d|\s|\,|\./)
+    // Only change code above this line
+  }
+  
+console.log(splitify("Hello World,I-am code"));
