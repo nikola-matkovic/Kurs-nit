27a0c0b012c4438503f3ddcc75cc287c60fb4207
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-16 17:55:14 +0200

    roman number converter

--- projects/roman-number-converter.js
+++ projects/roman-number-converter.js
@@ -4,15 +4,55 @@ function convertToRoman(num) {
         roman += "M";
         num -= 1000;
     }
-    while(num  / 500 >= 1){
-        roman += "D";
+    if(num  >= 900 ){
+        roman += "CM";
+        num -= 900;
+    }
+    if(num >= 500 ){
+        roman += "D"
         num -= 500;
     }
+    if(num >= 400 ){
+        roman += "CD"
+        num -= 400;
+    }
     while(num  / 100 >= 1){
-        roman += "M";
-        num -= 1000;
+        roman += "C";
+        num -= 100;
+    }
+    if(num  >= 90 ){
+        roman += "XC";
+        num -= 90;
+    }
+    if(num >= 50){
+        roman += "L"
+        num -= 50;
+    }
+    if(num >= 40){
+        roman += "XL"
+        num -= 40;
+    }
+    while(num  / 10 >= 1){
+        roman += "X";
+        num -= 10;
+    }
+    if(num  >= 9){
+        roman += "IX";
+        num -= 9;
+    }
+    if(num >= 5 ){
+        roman += "V"
+        num -= 5;
+    }
+    if(num  >= 4){
+        roman += "IV"
+        num -= 4;
+    }
+    while(num >= 1){
+        roman += "I"
+        num -= 1;
     }
     return roman;
 }
    
-console.log(convertToRoman(3000));
+console.log(convertToRoman(16));
