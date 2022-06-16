78d195f54d3cf5d0f4d3211b19f6c4d5a18b2b78
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-16 11:24:28 +0200

    square of integers wiht reduce method

---
+++ functional-programming/square of ints with reduce .js
@@ -0,0 +1,12 @@
+const squareList = arr => {
+  return arr.reduce((sqrIntegers, num) => {
+    return Number.isInteger(num) && num > 0
+      ? sqrIntegers.concat(num * num)
+      : sqrIntegers;
+  }, []);
+};
+
+
+
+const squaredIntegers = squareList([4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2]);
+console.log(squaredIntegers);
