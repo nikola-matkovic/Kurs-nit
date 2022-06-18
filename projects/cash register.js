1e2ad51d04cfbbc950b0533539405f1d3312dccb
anonymousvegan <majjunjulavgust@gmail.com>
2022-06-18 12:10:20 +0200

    transformed  2d array to object

--- projects/cash register.js
+++ projects/cash register.js
@@ -13,17 +13,13 @@ const currencyUnit = {
 function checkCashRegister(price, cash, cid) {
     let kusur = cash - price;
     let temp;
-    let finalObj = {status: "", charge : []};
-    for (let i in currencyUnit){
-        temp = 0;
-        while (kusur >= currencyUnit[i]){
-            kusur -= currencyUnit[i];
-            temp += currencyUnit[i];
-        }
-        if(temp){
-            finalObj.charge.push([i, temp])
-        };
-    }
+    let finalObj = {status: "OPEN", change : []};
+    //transoform cid to object
+    let cidObject = {}
+    cid.forEach((value) => {
+        cidObject[value[0]] = value[1];
+    })
+    console.log(cidObject)
     return finalObj;
 }
 
@@ -42,16 +38,16 @@ console.log(checkCashRegister(19.5,
 // {status: "OPEN", change: [["QUARTER", 0.5]]}
 
 
-// console.log(checkCashRegister(3.26,
-//     100,
-//     [   ["PENNY", 1.01], 
-//         ["NICKEL", 2.05], 
-//         ["DIME", 3.1], 
-//         ["QUARTER", 4.25], 
-//         ["ONE", 90], 
-//         ["FIVE", 55], 
-//         ["TEN", 20], 
-//         ["TWENTY", 60], 
-//         ["ONE HUNDRED", 100]
-//     ]))
+console.log(checkCashRegister(3.26,
+    100,
+    [   ["PENNY", 1.01], 
+        ["NICKEL", 2.05], 
+        ["DIME", 3.1], 
+        ["QUARTER", 4.25], 
+        ["ONE", 90], 
+        ["FIVE", 55], 
+        ["TEN", 20], 
+        ["TWENTY", 60], 
+        ["ONE HUNDRED", 100]
+    ]))
 // {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}
