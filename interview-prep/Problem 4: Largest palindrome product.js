eb689e35e082b88e275e89bde9a96a73b1df5ad3
anonymousvegan <majjunjulavgust@gmail.com>
2022-08-25 10:51:32 +0200

    bug fixes

--- interview-prep/Problem 4: Largest palindrome product.js
+++ interview-prep/Problem 4: Largest palindrome product.js
@@ -1,23 +1,25 @@
 function checkPalindrom(word){
     for(let i = 0; i < word.length; i++){
       if(word[i] != word[word.length-i-1]) {
-        console.log(i, word[i], word[word.length-1-i] )
         return false
       }
     } 
-    console.log(word.length)
     return true;
   }
   
   
   function largestPalindromeProduct(n) {
     let number = 0;
-    for(let i = 1; i < Math.pow(10, n); i++){
-      for(let j = 1; j < Math.pow(10, n); j++){
+    let max = 0;
+    for(let i =  Math.pow(10, n-1); i < Math.pow(10, n); i++){
+      for(let j =  Math.pow(10, n-1); j < Math.pow(10, n); j++){
         number = i * j;
-        checkPalindrom(number.toString())
+        if(checkPalindrom(number.toString()) && max < number){
+          max = number;
+        } 
       }
     }
+    return max;
   }
   
-  console.log(checkPalindrom(9009));
+console.log(largestPalindromeProduct(3));
